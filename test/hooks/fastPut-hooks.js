e7c74c9cd003fa309c169799ac9b77bbbff20a37
Tim Cross <theophilusx@gmail.com>
2019-03-04 03:21:32 +1100

    Breakout fastput tests into own file

---
+++ test/hooks/fastPut-hooks.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const {join} = require('path');
+
+function fastPutSetup(client, sftpUrl) {
+  return client
+    .put(Buffer.from('fast put'), join(sftpUrl, 'mocha-fastput.md'))
+    .then(() => {
+      return true;
+    })
+    .catch(err => {
+      throw new Error(`FastPut setup hook error: ${err.message}`);
+    });
+}
+
+function fastPutCleanup(client, sftpUrl) {
+  return client
+    .delete(join(sftpUrl, 'remote2.md.gz'))
+    .then(() => {
+      return client.delete(join(sftpUrl, 'remote.md'));
+    })
+    .catch(err => {
+      throw new Error(`FastPut cleanup hook error: ${err.message}`);
+    });
+}
+
+module.exports = {
+  fastPutSetup,
+  fastPutCleanup
+};
