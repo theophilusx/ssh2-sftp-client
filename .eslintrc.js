52e7741e7035344acbcae3a4ed55d9a16d5714be
Tim Cross <theophilusx@gmail.com>
2019-08-23 19:35:09 +1000

    Add editor config files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: 'should|expect'
+      }
+    ],
+    'no-console': 'off',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};
