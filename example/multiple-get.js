fde02b7cdb609ef983c25b9f26665e83932b8192
Tim Cross <theophilusx@gmail.com>
2019-09-18 13:17:01 +1000

    Added 2 examples of getting multiple files

---
+++ example/multiple-get.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const dotenvPath = __dirname + '/../.env';
+
+require('dotenv').config({path: dotenvPath});
+
+const Client = require('../src/index');
+const path = require('path');
+
+const client = new Client();
+
+const config = {
+  host: process.env.SFTP_SERVER,
+  username: process.env.SFTP_USER,
+  password: process.env.SFTP_PASSWORD,
+  port: process.env.SFTP_PORT || 22
+};
+
+async function downloadAll(remotePath, localPath) {
+  console.log(`Downloading files from ${remotePath}`);
+  await client.connect(config);
+  let listings = await client.list(remotePath, 'wftpserver*');
+  for (let item of listings) {
+    let remoteFile = path.join(remotePath, item.name);
+    let localFile = path.join(localPath, item.name);
+    console.log(`Remote: ${remoteFile} Local: ${localFile}`);
+    let res = await client.fastGet(remoteFile, localFile);
+    console.log(`${res} downloaded`);
+  }
+  await client.end();
+}
+
+downloadAll('/home/tim/testDownload', '/tmp')
+  .then(() => {
+    console.log('all files downloaded');
+  })
+  .catch(err => {
+    console.log(`An error occured: ${err.message}`);
+  });
---
+++ example/multiple-get2.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const dotenvPath = __dirname + '/../.env';
+
+require('dotenv').config({path: dotenvPath});
+
+const Client = require('../src/index');
+const path = require('path');
+
+const client = new Client();
+
+const config = {
+  host: process.env.SFTP_SERVER,
+  username: process.env.SFTP_USER,
+  password: process.env.SFTP_PASSWORD,
+  port: process.env.SFTP_PORT || 22
+};
+
+const remotePath = '/home/tim/testDownload';
+const localPath = '/tmp';
+
+client
+  .connect(config)
+  .then(() => {
+    return client.list(remotePath, 'wftpserver*');
+  })
+  .then(listing => {
+    let promises = [];
+    for (let item of listing) {
+      let remoteFile = path.join(remotePath, item.name);
+      let localFile = path.join(localPath, item.name);
+      console.log(`Remote: ${remoteFile} Local: ${localFile}`);
+      promises.push(client.fastGet(remoteFile, localFile));
+    }
+    return Promise.all(promises);
+  })
+  .then(rslts => {
+    rslts.forEach(r => console.log(`${r} downlaoded`));
+    return client.end();
+  })
+  .catch(err => {
+    console.log(`Error: ${err.message}`);
+  });
