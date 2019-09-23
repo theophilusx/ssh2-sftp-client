a3f4e8e963141ca6ce18eeaef7d667e62e4eb1ce
Tim Cross <theophilusx@gmail.com>
2019-09-24 09:10:14 +1000

    Added mkdir ezample script

---
+++ example/mkdir.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// sample mkdir use
+
+const dotenvPath = __dirname + '/../.env';
+
+require('dotenv').config({path: dotenvPath});
+
+const Client = require('../src/index');
+
+const config = {
+  host: process.env.SFTP_SERVER,
+  username: process.env.SFTP_USER,
+  password: process.env.SFTP_PASSWORD,
+  port: process.env.SFTP_PORT || 22
+};
+
+const path = '/tmp/testing/abc1/abc2';
+
+let client = new Client();
+
+client
+  .connect(config)
+  .then(() => {
+    console.log('Connected. Check if dir already exists');
+    return client.exists(path);
+  })
+  .then(alreadyExists => {
+    if (alreadyExists) {
+      console.log(`Path ${path} already exists!`);
+      return false;
+    } else {
+      console.log('Making directory');
+      return client.mkdir(path, true);
+    }
+  })
+  .then(rslt => {
+    console.log(`Result: ${rslt}`);
+    return client.end();
+  })
+  .catch(err => {
+    console.log(`Error: ${err.message}`);
+  });
