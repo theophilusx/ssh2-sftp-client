1a1a2c161d2dbc50627aa15ba95fce468c01a934
Tim Cross <theophilusx@gmail.com>
2020-08-28 12:25:32 +1000

    Update exist() tests for win32 platform

--- test/hooks/exist-hooks.js
+++ test/hooks/exist-hooks.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const {makeLocalPath, makeRemotePath} = require('./global-hooks');
+const {makeLocalPath} = require('./global-hooks');
 
 async function existSetup(client, sftpUrl, localUrl) {
   try {
-    await client.mkdir(makeRemotePath(sftpUrl, 'exist-test-dir'));
+    await client.mkdir(sftpUrl + '/exist-test-dir');
     await client.fastPut(
       makeLocalPath(localUrl, 'test-file1.txt'),
-      makeRemotePath(sftpUrl, 'exist-file.txt')
+      sftpUrl + '/exist-file.txt'
     );
     await client.fastPut(
       makeLocalPath(localUrl, 'test-file2.txt.gz'),
-      makeRemotePath(sftpUrl, 'exist-test-dir', 'exist-gzip.txt.gz')
+      sftpUrl + '/exist-test-dir/exist-gzip.txt.gz'
     );
     return true;
   } catch (err) {
@@ -22,8 +22,8 @@ async function existSetup(client, sftpUrl, localUrl) {
 
 async function existCleanup(client, sftpUrl) {
   try {
-    await client.delete(makeRemotePath(sftpUrl, 'exist-file.txt'));
-    await client.rmdir(makeRemotePath(sftpUrl, 'exist-test-dir'), true);
+    await client.delete(sftpUrl + '/exist-file.txt');
+    await client.rmdir(sftpUrl + '/exist-test-dir', true);
     return true;
   } catch (err) {
     console.error(`existCleanup: ${err.message}`);
