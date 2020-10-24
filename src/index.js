716b109875ae4ebdb6161435bbbcc197bdd3773f
Tim Cross <theophilusx@gmail.com>
2020-10-25 08:47:23 +1100

    Bumped moment and mocha versions

--- src/index.js
+++ src/index.js
@@ -838,11 +838,7 @@ class SftpClient {
       }
       return _rmdir(absPath);
     } catch (err) {
-<<<<<<< HEAD
-      return utils.handleError(err, 'rmdir');
-=======
       throw utils.formatError(err, 'rmdir', err.code);
->>>>>>> 79bbc2b... (WIP) Remove additional error checking
     }
   }
 
