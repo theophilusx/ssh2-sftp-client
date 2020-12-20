f32695bd764dbfcb007433f5497a19f00b218375
Tim Cross <theophilusx@gmail.com>
2020-12-21 08:36:14 +1100

    Update validation README and script

--- validation/fastput.js
+++ validation/fastput.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 'use strict';
 
 // A low level simple fastput script which only uses the underlying
@@ -10,32 +8,32 @@
 // 2 args src and dst. Script will upload src to dst on remote server.
 
 const path = require('path');
-
 const dotenvPath = path.join(__dirname, '..', '.env');
-
-console.log(`env path: ${dotenvPath}`);
 
 require('dotenv').config({path: dotenvPath});
 
-const Client = require('ssh2');
+const {Client} = require('ssh2');
 
 const config = {
   host: process.env.SFTP_SERVER,
   username: process.env.SFTP_USER,
   password: process.env.SFTP_PASSWORD,
-  port: process.env.SFTP_PORT || 22
+  port: process.env.SFTP_PORT || 22,
+  debug: (msg) => {
+    console.log(msg);
+  }
 };
 
 const options = {
-  mode: 0o777,
+  //mode: 0o777,
   //chunkSize: 32768,
-  concurrency: 1,
-  step: function(total_transferred, chunk, total) {
-    console.log(
-      `Total Transferred: ${total_transferred} Chunk: ${chunk}` +
-        ` Total: ${total}`
-    );
-  }
+  // concurrency: 1,
+  // step: function(total_transferred, chunk, total) {
+  //   console.log(
+  //     `Total Transferred: ${total_transferred} Chunk: ${chunk}` +
+  //       ` Total: ${total}`
+  //   );
+  // }
 };
 
 const src = process.argv[2];
@@ -43,23 +41,26 @@ const dst = process.argv[3];
 
 let client = new Client();
 
-client
-  .on('ready', function() {
-    client.sftp(function(err, sftp) {
+client.on('ready', () => {
+    client.sftp((err, sftp) => {
       if (err) {
-        throw err;
+        console.error(`SFTP Error: ${err.message}`);
+      } else {
+        console.log(`Transferring ${src} to ${dst}`);
+        sftp.fastPut(src, dst, options, (err) => {
+          if (err) {
+            console.error(`fastPut Error: ${err.message}`);
+          } else {
+            console.log('File uploaded');
+          }
+          client.end();
+        });
       }
-      console.log(`Transferring ${src} to ${dst}`);
-      sftp.fastPut(src, dst, options, function(err) {
-        if (err) {
-          throw err;
-        }
-        console.log('File uploaded');
-        client.end();
-      });
     });
-  })
-  .on('error', function(err) {
+  }).on('error', function(err) {
     console.error(err.message);
-  })
-  .connect(config);
+  }).on('end', () => {
+    console.log('End event fired');
+  }).on('close', () => {
+    console.log('Close event fired');
+  }).connect(config);
