78f90f917fed083f8c99cce37b963c091ce8032f
Tim Cross <theophilusx@gmail.com>
2023-02-19 11:34:19 +1100

    Cleanup of some tests

--- test/07rmdir.js
+++ test/07rmdir.js
@@ -26,27 +26,25 @@ describe('rmdir() method tests', function () {
   });
 
   it('rmdir should return a promise', function () {
-    return expect(sftp.rmdir(`${config.sftpUrl}/rmdir-promise`)).to.be.a(
-      'promise'
-    );
+    return expect(sftp.rmdir(`${config.sftpUrl}/rmdir-promise`)).to.be.a('promise');
   });
 
   it('rmdir on non-existent directory w/o recursion should be rejected', function () {
-    return expect(
-      sftp.rmdir(`${config.sftpUrl}/rmdir-not-exist`)
-    ).to.be.rejectedWith('No such file');
+    return expect(sftp.rmdir(`${config.sftpUrl}/rmdir-not-exist`)).to.be.rejectedWith(
+      'No such directory'
+    );
   });
 
   it('rmdir on non-existent directory w/ recursion should be rejected', function () {
     return expect(
       sftp.rmdir(`${config.sftpUrl}/rmdir-not-exist`, true)
-    ).to.be.rejectedWith('No such file');
+    ).to.be.rejectedWith('No such directory');
   });
 
   it('rmdir without recursion on empty directory', function () {
-    return expect(
-      sftp.rmdir(`${config.sftpUrl}/rmdir-empty`)
-    ).to.eventually.equal('Successfully removed directory');
+    return expect(sftp.rmdir(`${config.sftpUrl}/rmdir-empty`)).to.eventually.equal(
+      'Successfully removed directory'
+    );
   });
 
   it('rmdir recursively remove all directories', function () {
@@ -62,15 +60,13 @@ describe('rmdir() method tests', function () {
   });
 
   it('rmdir with relative path 1', function () {
-    return expect(
-      sftp.rmdir('./testServer/rmdir-relative1')
-    ).to.eventually.equal('Successfully removed directory');
+    return expect(sftp.rmdir('./testServer/rmdir-relative1')).to.eventually.equal(
+      'Successfully removed directory'
+    );
   });
 
   it('rmdir with relative path 2', function () {
-    let remotePath = `../${lastRemoteDir(
-      config.remoteRoot
-    )}/testServer/rmdir-relative2`;
+    let remotePath = `../${lastRemoteDir(config.remoteRoot)}/testServer/rmdir-relative2`;
     return expect(sftp.rmdir(remotePath)).to.eventually.equal(
       'Successfully removed directory'
     );
