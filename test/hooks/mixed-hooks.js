aa21014cb17031cf58be89c619f3c9ca7f30133c
Tim Cross <theophilusx@gmail.com>
2023-04-15 17:14:34 +1000

    Throw errors in test hooks rather than just log

--- test/hooks/mixed-hooks.js
+++ test/hooks/mixed-hooks.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {makeLocalPath, makeRemotePath} = require('./global-hooks');
+const { makeLocalPath, makeRemotePath } = require('./global-hooks');
 
 async function realpathSetup(client, sftpUrl, localUrl) {
   try {
@@ -15,8 +15,7 @@ async function realpathSetup(client, sftpUrl, localUrl) {
     );
     return true;
   } catch (err) {
-    console.error(`mixedSetup: ${err.message}`);
-    return false;
+    throw new Error(`mixedSetup: ${err.message}`);
   }
 }
 
@@ -25,105 +24,92 @@ async function realpathCleanup(client, sftpUrl) {
     await client.rmdir(makeRemotePath(sftpUrl, 'path-test-dir'), true);
     return true;
   } catch (err) {
-    console.log(`mixedClenaup: ${err.message}`);
-    return false;
+    throw new Error(`mixedClenaup: ${err.message}`);
   }
 }
 
 async function listSetup(client, sftpUrl, localUrl) {
   try {
-    await client.mkdir(sftpUrl + '/list-test/dir1', true);
-    await client.mkdir(sftpUrl + '/list-test/dir2/sub1', true);
-    await client.mkdir(sftpUrl + '/list-test/empty', true);
+    await client.mkdir(`${sftpUrl}/list-test/dir1`, true);
+    await client.mkdir(`${sftpUrl}/list-test/dir2/sub1`, true);
+    await client.mkdir(`${sftpUrl}/list-test/empty`, true);
     await client.put(
       Buffer.from('<title>List Test Data 1</title>'),
-      sftpUrl + '/list-test/file1.html',
-      {encoding: 'utf8'}
+      `${sftpUrl}/list-test/file1.html`,
+      { encoding: 'utf8' }
     );
-    await client.put(
-      Buffer.from('# List Test Data 2'),
-      sftpUrl + '/list-test/file2.md',
-      {encoding: 'utf8'}
-    );
+    await client.put(Buffer.from('# List Test Data 2'), `${sftpUrl}/list-test/file2.md`, {
+      encoding: 'utf8',
+    });
     await client.fastPut(
       makeLocalPath(localUrl, 'test-file1.txt'),
-      sftpUrl + '/list-test/test-file1.txt',
-      {encoding: 'utf8'}
+      `${sftpUrl}/list-test/test-file1.txt`,
+      { encoding: 'utf8' }
     );
     await client.fastPut(
       makeLocalPath(localUrl, 'test-file2.txt.gz'),
-      sftpUrl + '/list-test/test-file2.txt.gz'
+      `${sftpUrl}/list-test/test-file2.txt.gz`
     );
     return true;
   } catch (err) {
-    console.error(`listSetup: ${err.message}`);
-    return false;
+    throw new Error(`listSetup: ${err.message}`);
   }
 }
 
 async function listCleanup(client, sftpUrl) {
   try {
-    await client.rmdir(sftpUrl + '/list-test', true);
+    await client.rmdir(`${sftpUrl}/list-test`, true);
     return true;
   } catch (err) {
-    console.error(`listCleanup: ${err.message}`);
-    return false;
+    throw new Error(`listCleanup: ${err.message}`);
   }
 }
 
 async function existSetup(client, sftpUrl, localUrl) {
   try {
-    await client.mkdir(sftpUrl + '/exist-test-dir');
+    await client.mkdir(`${sftpUrl}/exist-test-dir`);
     await client.fastPut(
       makeLocalPath(localUrl, 'test-file1.txt'),
-      sftpUrl + '/exist-file.txt'
+      `${sftpUrl}/exist-file.txt`
     );
     await client.fastPut(
       makeLocalPath(localUrl, 'test-file2.txt.gz'),
-      sftpUrl + '/exist-test-dir/exist-gzip.txt.gz'
+      `${sftpUrl}/exist-test-dir/exist-gzip.txt.gz`
     );
     return true;
   } catch (err) {
-    console.error(`existSetup: ${err.message}`);
-    return false;
+    throw new Error(`existSetup: ${err.message}`);
   }
 }
 
 async function existCleanup(client, sftpUrl) {
   try {
-    await client.delete(sftpUrl + '/exist-file.txt', true);
-    await client.rmdir(sftpUrl + '/exist-test-dir', true);
+    await client.delete(`${sftpUrl}/exist-file.txt`, true);
+    await client.rmdir(`${sftpUrl}/exist-test-dir`, true);
     return true;
   } catch (err) {
-    console.error(`existCleanup: ${err.message}`);
-    return false;
+    throw new Error(`existCleanup: ${err.message}`);
   }
 }
 
 async function statSetup(client, sftpUrl) {
   try {
-    await client.put(
-      Buffer.from('# Stat test data'),
-      sftpUrl + '/stat-test.md',
-      {
-        encoding: 'utf8',
-        mode: 0o777
-      }
-    );
+    await client.put(Buffer.from('# Stat test data'), `${sftpUrl}/stat-test.md`, {
+      encoding: 'utf8',
+      mode: 0o777,
+    });
     return true;
   } catch (err) {
-    console.error(`statSetup: ${err.message}`);
-    return false;
+    throw new Error(`statSetup: ${err.message}`);
   }
 }
 
 async function statCleanup(client, sftpUrl) {
   try {
-    await client.delete(sftpUrl + '/stat-test.md', true);
+    await client.delete(`${sftpUrl}/stat-test.md`, true);
     return true;
   } catch (err) {
-    console.error(`statCleanup: ${err.message}`);
-    return false;
+    throw new Error(`statCleanup: ${err.message}`);
   }
 }
 
@@ -135,5 +121,5 @@ module.exports = {
   existSetup,
   existCleanup,
   statSetup,
-  statCleanup
+  statCleanup,
 };
